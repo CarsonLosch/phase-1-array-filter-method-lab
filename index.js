e59fe3ab466dcd5e3e27408e4d1a60c2a9472dbd
CarsonLosch <95552371+CarsonLosch@users.noreply.github.com>
2022-03-11 10:00:45 -0800

    commit tree 2e830346c4de9b1c3de5858636eb67f702a29cb5

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+// Code your solution here  
+
+const findMatching = function(drivers, string) {
+    const result = drivers.filter(driversName => driversName.toLowerCase() === string.toLowerCase())
+    return result;
+}
+
+const fuzzyMatch = function(drivers, string) {
+    const result = drivers.filter(driversName => driversName.charAt() === string.charAt())
+    return result;
+}
+
+const matchName = function(drivers, string) {
+    const result = drivers.filter(driversName => driversName.name === string)
+    return result
+}
